ae23f9d35f21a0828843f60a86d86902f3670560
hongguoqiang <2310087518@qq.com>
2024-04-07 16:22:02 +0800

    feat: 完善eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  rules: {
+    'no-var': 2, // 禁用var，用let和const代替
+    strict: 2, // 使用严格模式
+    'react/react-in-jsx-scope': 'off', // 自 React 17 起，在作用域中不需要引入React
+    'react-native/no-unused-styles': 'warn', // 警告未使用的样式
+    'react-native/split-platform-components': 'warn', // 当必要时强制使用特定于平台的文件
+    'react-hooks/rules-of-hooks': 'error', // 检查Hooks规则
+    'react-hooks/exhaustive-deps': 'warn', // 检查effect依赖
+    'jsx-quotes': ['error', 'prefer-double'], // 强制在JSX属性中使用双引号
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  arrowParens: 'avoid', // 在箭头函数只有一个参数的时候避免使用括号
+  bracketSameLine: true, // 在对象字面量的括号和对象的第一个属性在同一行
+  bracketSpacing: false, // 对象字面量的括号前后不加空格
+  singleQuote: true, // 使用单引号
+  trailingComma: 'all', // 多行对象字面量中加逗号
+  printWidth: 160, // 最大行宽
 };
