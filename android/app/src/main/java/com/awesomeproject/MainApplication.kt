9623402ba12894760ee7e17cd88cf817a6cff9c9
hongguoqiang <2310087518@qq.com>
2024-04-08 17:55:34 +0800

    🌈 style(rename):

--- android/app/src/main/java/com/awesomeproject/MainApplication.kt
+++ android/app/src/main/java/com/awesomeproject/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.awesomeproject
+package com.ledger
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/AwesomeProjectTests/AwesomeProjectTests.m
+++ ios/AwesomeProjectTests/AwesomeProjectTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AwesomeProjectTests : XCTestCase
+@interface ledgerTests : XCTestCase
 
 @end
 
-@implementation AwesomeProjectTests
+@implementation ledgerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
